6e4540ba53ae6e39a6cfb793f644eaf4f92f1cce
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-04-03 17:07:57 +0500

    Removed an old implementation of results()

--- dij/polls/views.py
+++ dij/polls/views.py
@@ -17,10 +17,6 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 
-def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}.")
-
-
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
