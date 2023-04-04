c214dd95561b6796e428bfdf74d7df72a206fd1a
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-04-04 17:16:34 +0500

    Renamed 'dij' folders to 'did'
    DID stands for 'dive into Django'

--- did/did/urls.py
+++ did/did/urls.py
@@ -1,4 +1,4 @@
-"""dij URL Configuration
+"""did URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- did/did/wsgi.py
+++ did/did/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dij project.
+WSGI config for did project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dij.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'did.settings')
 
 application = get_wsgi_application()
