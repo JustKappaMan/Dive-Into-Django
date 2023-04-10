8b2927b874242935baa07c7eec523802b00d23c0
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-04-10 23:45:14 +0500

    Tiny refactoring
    Renamed several variables

--- did/polls/tests.py
+++ did/polls/tests.py
@@ -12,8 +12,8 @@ class QuestionModelTests(TestCase):
         was_published_recently() returns False for questions whose pub_date
         is in the future.
         """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
+        future = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=future)
         self.assertIs(future_question.was_published_recently(), False)
 
     def test_was_published_recently_with_old_question(self):
@@ -21,8 +21,8 @@ class QuestionModelTests(TestCase):
         was_published_recently() returns False for questions whose pub_date
         is older than 1 day.
         """
-        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-        old_question = Question(pub_date=time)
+        past = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=past)
         self.assertIs(old_question.was_published_recently(), False)
 
     def test_was_published_recently_with_recent_question(self):
@@ -30,6 +30,6 @@ class QuestionModelTests(TestCase):
         was_published_recently() returns True for questions whose pub_date
         is within the last day.
         """
-        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-        recent_question = Question(pub_date=time)
+        recent = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=recent)
         self.assertIs(recent_question.was_published_recently(), True)
